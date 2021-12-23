097597ef19a7f6c406916f6c4d9af62b42206118
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-23 19:37:16 +0100

    [website.config.js] Keep adding fields

--- website.config.js
+++ website.config.js
@@ -1,18 +1,37 @@
 module.exports = {
-  github_account: 'JulesFouchy',
-  repo: 'Learn--Math-for-Art-and-Computer-Graphics',
-  docusaurus_settings: {
-    title: 'Mathematics for Art',
-    tagline: 'Making 🎨 with 👩‍💻',
+  github_account: "JulesFouchy",
+  repo: "Learn--Math-for-Art-and-Computer-Graphics",
+  docusaurus_config: {
+    title: "Mathematics for Art",
+    tagline: "Making 🎨 with 👩‍💻",
+    // Override any parts of docusaurus.config.js you want
   },
   navbar_items: [
     {
-      type: 'doc',
-      docId: 'lessons/introduction',
-      position: 'left',
-      label: 'Lessons',
+      type: "doc",
+      docId: "lessons/introduction",
+      position: "left",
+      label: "Lessons",
     },
-    {to: '/resources', label: 'Resources', position: 'left'},
-    {to: '/inspirations', label: 'Inspirations', position: 'left'},
+    { to: "/resources", label: "Resources", position: "left" },
+    { to: "/inspirations", label: "Inspirations", position: "left" },
   ],
-}
+  //   footer: {}, // Uncomment this to override the hardcoded footer in docusaurus.config.js
+  contact_info: [
+    {
+      label: "Discord",
+      to: "https://discord.com/users/372812330742054914",
+    },
+    {
+      label: "E-Mail",
+      to: "mailto:jules.fouchy@ntymail.com",
+    },
+    {
+      label: "GitHub",
+      to: "https://github.com/JulesFouchy/JulesFouchy/issues",
+    },
+  ],
+  extra_copyright: "",
+  syntax_highlight_for: ["typescript"],
+  //   path_to_docs: "../../docs", uncomment this to change the path to the docs folder expected by Docusaurus. The path should be relative to Teaching-Toolbox/website
+};
