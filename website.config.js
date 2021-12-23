7b50b626e71caf392380b96a119abff9db0bfb70
Jules Fouchy <jules.fouchy@ntymail.com>
2021-12-23 19:01:49 +0100

    Start website.config.js

---
+++ website.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  github_account: 'JulesFouchy',
+  repo: 'Learn--Math-for-Art-and-Computer-Graphics',
+  docusaurus_settings: {
+    title: 'Mathematics for Art',
+    tagline: 'Making 🎨 with 👩‍💻',
+  },
+  navbar_items: [
+    {
+      type: 'doc',
+      docId: 'lessons/introduction',
+      position: 'left',
+      label: 'Lessons',
+    },
+    {to: '/resources', label: 'Resources', position: 'left'},
+    {to: '/inspirations', label: 'Inspirations', position: 'left'},
+  ],
+}
